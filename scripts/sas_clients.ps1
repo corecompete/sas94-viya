1c4a1fafeacca3171dc7d89c63c0e5d57fa057c7
manoj.kotrike@corecompete.com <manoj.kotrike@corecompete.com>
2020-09-14 13:52:52 +0100

    Fixed error handling of SAS clients installation and access policy

--- scripts/sas_clients.ps1
+++ scripts/sas_clients.ps1
@@ -7,15 +7,26 @@ param (
 	[string]$app_name,
 	[string]$mid_name,
 	[string]$domain_name,
-	[string]$artifact_loc
+	[string]$artifact_loc,
+	$code=99
 )
-
+#Function for Error Handling
+function ExitWithCode { 
+if($? -eq "true"){
+  write-host("Commnad executed successfully")
+ }else {
+  write-host("Commnad execution failed ")
+  exit $code
+  } 
+}
 Set-PSDebug -Trace 1;
 $logdir = "C:\saslog"
 $mid_fqdn= "${app_name}${mid_name}.${domain_name}"
 New-Item -Path $logdir -ItemType directory
 Invoke-WebRequest -Uri ${artifact_loc}properties/clients_install.properties -OutFile ${logdir}\clients_install.properties
+ExitWithCode
 Invoke-WebRequest -Uri ${artifact_loc}properties/plan.xml -OutFile ${logdir}\plan.xml
+ExitWithCode
 (Get-Content -path ${logdir}\clients_install.properties -Raw) -replace 'client_sid',$clients_sid | Add-Content -Path ${logdir}\clients_install_new.properties
 Remove-Item -Path ${logdir}\clients_install.properties
 Move-Item -Path ${logdir}\clients_install_new.properties -Destination ${logdir}\clients_install.properties
@@ -25,7 +36,6 @@ Move-Item -Path ${logdir}\clients_install_new.properties -Destination ${logdir}\
 (Get-Content -path ${logdir}\clients_install.properties -Raw) -replace 'mid_fqdn',$mid_fqdn | Add-Content -Path ${logdir}\clients_install_new.properties
 Remove-Item -Path ${logdir}\clients_install.properties
 Move-Item -Path ${logdir}\clients_install_new.properties -Destination ${logdir}\clients_install.properties
-
 $connectTestResult = Test-NetConnection -ComputerName "${stg_acc_name}.file.core.windows.net" -Port 445
 
 if ($connectTestResult.TcpTestSucceeded) {
@@ -37,23 +47,20 @@ if ($connectTestResult.TcpTestSucceeded) {
 } else {
     Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
 }
-
-
-cd "Z:\${depot_folder_name}"
+Set-Location "Z:\${depot_folder_name}"
 .\setup.exe -lang en -deploy -datalocation C:\saslog -responsefile ${logdir}\clients_install.properties -quiet 
+ExitWithCode
 Start-Sleep -Seconds 1200
-
 $latest = Get-ChildItem -Path ${logdir}\deployw* | Sort-Object LastAccessTime -Descending | Select-Object -First 1
 $latest.name
-cd $logdir
+Set-Location $logdir
 $sort_string = Select-String -Path $latest.name -Pattern "ExitInstance="
-$alert = $sort_string | Select-String -pattern "ExitInstance=0" -notMatch
-if ($alert) { 
-	Install Failed
-    throw "Install Is Failed"
+$status = $sort_string.Line.split() | ForEach-Object {$_.substring($_.length-1)} | Select-Object -first 1 -skip 1 
+if ($status -ne 0) { 
+    Write-Host "Install Is Failed"
+    exit $code
     }
 else {
     Write-Host "Install Is Sucess"
-}
-
+ }
 $Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
