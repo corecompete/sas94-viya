1e9adcf94d56508bbdd64ff7c2b9a89d2e53c52e
cc-sshaik <shaharaz.shaik@corecompete.com>
2020-11-07 21:56:55 +0530

    Modified the file structure

--- scripts/sas_clients.ps1
+++ scripts/sas_clients.ps1
@@ -22,13 +22,10 @@ if($? -eq "true"){
 Set-PSDebug -Trace 1;
 $logdir = "C:\saslog"
 $mid_fqdn= "${app_name}${mid_name}.${domain_name}"
-$client_uri= "https://raw.githubusercontent.com/corecompete/sasinstalls/main/clients_install.properties"
-$plan_uri= "https://raw.githubusercontent.com/corecompete/sasinstalls/main/plan.xml"
-
 New-Item -Path $logdir -ItemType directory
-Invoke-WebRequest -Uri $client_uri -OutFile ${logdir}\clients_install.properties
+Invoke-WebRequest -Uri ${artifact_loc}clients_install.properties -OutFile ${logdir}\clients_install.properties
 ExitWithCode
-Invoke-WebRequest -Uri $plan_uri -OutFile ${logdir}\plan.xml
+Invoke-WebRequest -Uri ${artifact_loc}plan.xml -OutFile ${logdir}\plan.xml
 ExitWithCode
 (Get-Content -path ${logdir}\clients_install.properties -Raw) -replace 'client_sid',$clients_sid | Add-Content -Path ${logdir}\clients_install_new.properties
 Remove-Item -Path ${logdir}\clients_install.properties
